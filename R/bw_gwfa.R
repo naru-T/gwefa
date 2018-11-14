29df3d0e225ad9661df9eda97e6329800c4315fd
naru-T <narumida@gmail.com>
2018-11-14 18:01:35 +0900

    lower limit has been changed

--- R/bw_gwfa.R
+++ R/bw_gwfa.R
@@ -40,7 +40,7 @@ bw_gwfa <- function(data, vars,k=2, scores, robust=FALSE, kernel, adaptive=TRUE,
   var.n <- ncol(data)
   if (adaptive) {
     upper <- dp.n
-    lower <- 20 ##chenged from 2 to 20
+    lower <- dp.n/3 ##chenged from 2 to dp.n/3
   }
   else {
     if (DM.given) {
