474bcc24913ab4afe7556eaaa12f78e8b82c4eed
rsnaru <narumida@gmail.com>
2019-01-10 17:04:46 +0900

    foreach option added!

--- R/gwfa.R
+++ R/gwfa.R
@@ -3,13 +3,15 @@ gwfa <- function(data,elocat, vars,bw,k=2, kernel, adaptive=TRUE, p=2, theta=0, 
                   residuals=FALSE, SMC=TRUE, covar=FALSE,missing=FALSE,impute="median",
                   min.err = 0.001,  max.iter = 50,symmetric=TRUE, warnings=TRUE, fm="minres",
                   alpha=.1,pr=.05,oblique.scores=FALSE,np.obs=NULL,use="pairwise",cor="cor",
-                  correct=.5,weight=NULL, timeout,...) {
+                  correct=.5,weight=NULL, timeout, foreach=FALSE, core=NA, ...) {
 
 
 ##This function is based on GWmodel::gwpca and psych::fa
   requireNamespace("GWmodel")
   requireNamespace("psych")
   requireNamespace("R.utils")
+  requireNamespace("foreach")
+  requireNamespace("doMC")
 
   if (is(data, "Spatial")) {
     p4s <- proj4string(data)
@@ -70,6 +72,91 @@ gwfa <- function(data,elocat, vars,bw,k=2, kernel, adaptive=TRUE, p=2, theta=0, 
   if (len.var > var.n)
     warning("Invalid variables have been specified, please check them again!")
 
+  if(foreach == TRUE){
+    
+    registerDoMC(core)
+    out <- foreach(i= 1:ep.n) %dopar% {
+      #for (i in 1:ep.n) {
+      
+      score.all <-  matrix(NA,ep.n,k)
+      
+      if (DM.given)
+        dist.vi <- dMat[, i]
+      else {
+        if (ep.given)
+          dist.vi <- gw.dist(dp.locat, elocat, focus = i,
+                             p, theta, longlat)
+        else dist.vi <- gw.dist(dp.locat, focus = i, p = p,
+                                theta = theta, longlat = longlat)
+      }
+      wt <- gw.weight(dist.vi, bw, kernel, adaptive)
+      use <- wt > 0
+      wt <- wt[use]
+      if (length(wt) <= 5) {
+        expr <- paste("Too small bandwidth at location: ",
+                      i)
+        warning(paste(expr, "and the results can't be given there.",
+                      sep = ", "))
+        next
+      }
+      
+      temp <- wfa(x=data[use, ], wt, factors=k, scores=scores, n.obs, fm=fm, rotate=rotate, oblique.scores=oblique.scores, timeout=timeout)
+      
+      
+      if(is.null(temp)){
+        load <- NA
+        score.all <- NA
+        s <- NA
+        u <- NA
+        ld<- NA
+        ss <- NA
+        cortemp <- NA
+        cor.mt <- NA
+        resid_sqsum <- NA
+        rmsea <- NA
+        
+      } else {
+        
+        colnm <- colnames(temp$scores)
+        load <- matrix(temp$loadings[, order(colnm)], ncol=k, nrow=var.n)
+        #score
+        
+        score.all[use, ] <- temp$scores[, order(colnm)]
+        s <- score.all[i,]
+        u <- temp$uniquenesses
+        ld<- temp$Vaccounted[1,order(colnm)]
+        ss <- temp$Vaccounted[2,order(colnm)]
+        cortemp <- cor(temp$scores[,order(colnm)])
+        cor.mt <- cortemp[upper.tri(cortemp)] #correlation between factor scores
+        resid_sqsum <- sum(temp$residual[upper.tri(temp$residual)]^2, na.rm=TRUE)
+        rmsea <- temp$RMSEA[1]
+      }
+      
+      list(
+        loadings=load,
+        score=s,
+        uniquenesses=u,
+        loading_score=ld,
+        residuals_sqsum = resid_sqsum,
+        ss=ss,
+        cor.mt=cor.mt,
+        rmsea=rmsea)
+    }
+    
+    res <- list(
+      # loadings = lapply(out,"[[","loadings") %>% unlist()
+      loadings = array(as.numeric(unlist(lapply(out,"[[","loadings"))), dim=c(c(var.n,k,ep.n))) %>% aperm(.,  c(3,1,2)),
+      score = t(sapply(out,"[[", 'score')),
+      uniquenesses = t(sapply(out,"[[","uniquenesses")),
+      loading_score = t(sapply(out,"[[","loading_score")),
+      residuals_sqsum = t(sapply(out,"[[","residuals_sqsum")) %>% as.numeric(),
+      ss = t(sapply(out,"[[","ss")),
+      cor.mt = t(sapply(out,"[[","cor.mt")),
+      rmsea = t(sapply(out,"[[","rmsea")) %>% as.numeric()
+    )
+    
+    
+  } else{
   load <- array(NA,c(ep.n,var.n,k))
   resid_sqsum <- rep(NA, nrow(elocat))
   s <- matrix(NA,ep.n,k)
@@ -134,13 +221,17 @@ gwfa <- function(data,elocat, vars,bw,k=2, kernel, adaptive=TRUE, p=2, theta=0, 
     }
   }
   dimnames(load)[[3]] <- colnames(temp$loadings)[order(colnames(temp$loadings))]
-  list(
-    loadings=load,
-    score=s,
-    uniquenesses=u,
-    loading_score=ld,
-    residuals_sqsum = resid_sqsum,
-    ss=ss,
-    cor.mt=cor.mt,
-    rmsea=rmsea)
+ res <- list(
+        loadings=load,
+        score=s,
+        uniquenesses=u,
+        loading_score=ld,
+        residuals_sqsum = resid_sqsum,
+        ss=ss,
+        cor.mt=cor.mt,
+        rmsea=rmsea)
+  }
+  
+  return(res)
+  
 }
